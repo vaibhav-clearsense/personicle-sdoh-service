c8dc8351856830cc536c170fc3d1a3e7762872c4
Narendra Badam <nbadam@clearsense.com>
2022-08-17 17:25:16 -0400

    Aug17sdohfulldataformatupdate

--- sdoh_datatransformation_insurance.py
+++ sdoh_datatransformation_insurance.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug  2 16:36:51 2022
+Created on Mon Aug 15 20:23:57 2022
 
 @author: nbadam
 """
 
-from pyforest import *
+import numpy as np
+import pandas as pd
 
 from base_schema import *
 from db_connection import *
-
-from flask import Flask
-from flask import request
 
 from sqlalchemy import select
 import pandas.io.sql as sqlio
@@ -22,14 +20,48 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
 
+df=pd.read_csv('healthinsurance_sdoh1.csv',encoding="utf8")
+
+df.columns = df.iloc[0]
+df = df[1:].copy()
 
 
-df=pd.read_csv('healthinsurance_sdoh.csv',encoding="utf8")
 
-df.rename(columns={'Label (Grouping)':'label'},inplace=True)
-df.columns = [col.replace('\xa0', '') for col in df.columns]
-df1=df.melt(id_vars='label')
-df1 =df1.replace(u'\xa0', u'', regex=True).sort_values(by='label')
+df.drop(columns=['id'],inplace=True)
+df.rename(columns={'Geographic Area Name':'zip'},inplace=True)
+df["zip"]=df["zip"].str.replace("ZCTA5 ","")
+
+
+collist1=df.filter(like='19 to 64 years').columns.tolist()
+collist2=df.filter(like='poverty').columns.tolist()
+collist3=df.filter(like='CITIZENSHIP STATUS').columns.tolist()
+collist4=df.filter(like='poverty threshold').columns.tolist()
+
+collist=collist1+collist2+collist3+collist4
+
+
+
+
+df.drop(columns=collist,inplace=True)
+
+#df=df.head(50)
+
+df1=df.melt(id_vars=['zip'], var_name='variable', value_name='value')
+
+
+df1 =df1.replace(u'\xa0', u'', regex=True)#.sort_values(by='label')
+
+
+df2=pd.concat([df1[['zip','value']], df1['variable'].str.split('!!', expand=True)], axis=1)
+
+df2=df2[(df2[4].notnull())|(df2[5].isin(['Under $25,000', '$25,000 to $49,999',
+       '$50,000 to $74,999', '$75,000 to $99,999', '$100,000 and over']))].drop(columns=[2,3,6])
+
+df2['label']=df2[4].astype(str)+df2[5].astype(str)
+
+df2["label"]=df2["label"].str.replace("None","").str.replace("Civilian noninstitutionalized population","").str.replace("Total household population","")
+df2.drop(columns=[4,5],inplace=True)
+df2.rename(columns={1:'insurance',0:'parameters'},inplace=True)
 
 dct_cat={ '19 to 25 years':'age',
        '19 to 64 years':'age', '26 to 34 years':'age', '35 to 44 years':'age',
@@ -52,12 +84,7 @@ dct_cat={ '19 to 25 years':'age',
          '$50,000 to $74,999':'hh income','$75,000 to $99,999':'hh income','$100,000 and over':'hh income'    
         }
 
-df2=pd.concat([df1[['label','value']], df1['variable'].str.split('!!', expand=True)], axis=1)
-df2.rename(columns={0:'zip',1:'insurance',2:'parameters'},inplace=True)
-
 df2['category']=df2.label.map(dct_cat)
-df2["zip"]=df2["zip"].str.replace("ZCTA5 ","")
-
 df_temp=df2[(df2.category.notnull())&((df2.insurance=='Insured')|(df2.insurance=='Uninsured'))&(df2.insurance!='Total')]
 
 df_temp["value"]=df_temp["value"].astype(str)
@@ -80,12 +107,16 @@ df_temp.replace('$25,000 to $49,999', '25000-49999', inplace=True)
 df_temp.replace('$50,000 to $74,999', '50000-74999', inplace=True)
 df_temp.replace('$75,000 to $99,999', '75000-99999', inplace=True)
 
-df_temp['value']=df_temp['value'].astype(float)
-
 #Renaming columns
 df_temp.rename(columns={'insurance':'outputparametername','parameters':'outputparametertype','value':'outputvalue'},inplace=True)
+
+#Cleaning zipcodes
+df_temp["zip"]=df_temp["zip"].str.replace("ZCTA5 ","")
+
+#print(df_temp.dtypes)
+
+df_temp['outputvalue']=df_temp['outputvalue'].astype(float)
 
 
 
 df_temp.to_sql(name='insdata', con=engine, if_exists='replace', index=False)
-
